d7cd3e305bcf7c15d12e5f0a4cc9a9500ae0ca09
Daniel <Cardenas>
2020-07-16 08:47:28 -0400

    DCN: Add logical oeprator

---
+++ python_cookbook/logical_operators.py
@@ -0,0 +1,31 @@
+x = 2
+
+################
+# Bad Practice #
+################
+
+if x == 0:
+    print('a')
+    pass
+elif x == 1:
+    print('b')
+    pass
+elif x == 2:
+    print('c')
+    pass
+elif x == 3:
+    print('d')
+    pass
+
+#################
+# Good Practice #
+#################
+
+x_states_text = {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+    3: 'd'
+}
+
+print(x_states_text[x])
